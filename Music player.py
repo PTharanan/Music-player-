572e90f56f53c8b48dc6f41b8414c574e358ce65
Tharanan <ptharanan@gmail.com>
2023-11-19 21:03:05 +0530

    Add files via upload

---
+++ Music player.py
@@ -0,0 +1,11 @@
+# Use python lib
+import playsound
+# Set music file same parth
+music = ('Uyire-Un-Uyirena.mp3', 'Vaa-Vasuki-MassTamilan.io.mp3', 'Vizhigalile-MassTamilan.com.mp3')
+while True:
+    # Song start
+    print('Enjoy....')
+    # play music
+    playsound.playsound(music[2], True)
+    # Song end
+    print('......End')
